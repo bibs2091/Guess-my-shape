5b8eb45d607bf204cb44cff339c83a390999df20
bibs2091 <m.naas@esi-sba.dz>
2021-02-06 01:39:45 +0100

    next word feature

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def predict_sketch(img):
     pred,pred_idx,probs = learner.predict(img)
     predictions = sorted(zip(classes, map(float, probs)), key=lambda p: p[1], reverse=True)
     # print(predictions[:10])
-    return predictions[0]
+    return predictions
 
 # load the learner
 learner = load_posix_learner()
@@ -37,7 +37,9 @@ def predict():
         image = Image.open(io.BytesIO(base64_decoded)).convert('L')
         image.thumbnail( [28,28],Image.ANTIALIAS )
         image_np = np.asarray(image)
-        sketch_name, prob = predict_sketch(image_np)
-        return sketch_name
+        predictions = predict_sketch(image_np)
+        return jsonify(
+            answers=[predictions[0][0],predictions[1][0]]
+        )
     return render_template("index.html")
 
--- static/script.js
+++ static/script.js
@@ -1,12 +1,11 @@
 var radius;
 var c;
+var StartedDraing = false;
 
 function setup() {
   var myCanvas = createCanvas(300, 300);
   myCanvas.parent("myCanvas");
   createP();
-  // eraser = createButton("clear");
-  // eraser.mousePressed(clearPaint);
   c = color(255);
   background(0);
 }
@@ -19,13 +18,14 @@ function mouseDragged() {
   stroke(c)
   strokeWeight(10);
   line(mouseX, mouseY, pmouseX, pmouseY);
+  StartedDraing = true
 }
-
 
 function game() {
   var savedData = new Image();
   var canvas = document.getElementsByTagName('canvas')[0];
   var timer = 0
+  var foundIt = false
   var intervalId = window.setInterval(function () {
     $.ajax({
       url: '/',
@@ -33,27 +33,60 @@ function game() {
       data: canvas.toDataURL("image/png").replace('data:image/png;base64,', ''),
       success: function (response) {
         $(".game p").show()
-        $("#sketch-name").text(response)
-        console.log(response)
+        var count = 0
+        var handle = setInterval( function() {
+           if (response.answers[count] == word.textContent && StartedDraing == true){
+              $(".game p #ai").text("AI : I found it !!!, it's ")
+              $("#sketch-name").text(response.answers[count])
+              foundIt = true
+              console.log("right answer")
+              nextWord()
+              clearInterval(handle)
+            }
+            else if(!foundIt && StartedDraing){
+                console.log("wrong :() answer")
+                $('#sketch-name').text(response.answers[count])
+            }
+          count++;
+          if (count>=2) clearInterval(handle);
+
+        }, 2500 );
+
+        
       }
     })
-    timer += 1;
 
+    
+
+    timer += 1;
     if (timer == 4) {
-      let word = classes[Math.floor(Math.random() * classes.length)];
-      $("#word").text(word);
-      background(0)
-      timer = 0
+      nextWord()
     }
-  }, 4000);
+  }, 5000);
+}
+
+function nextWord(){
+  word = classes[Math.floor(Math.random() * classes.length)];
+  $("#word").text(word);
+  $(".game p").hide()
+  $(".game p #ai").text("AI : is it ")
+  $("#sketch-name").text("")
+  background(0)
+  timer = 0
+  foundIt = false
+  StartedDraing = false
 }
 
 $(document).ready(function () {
+  var word
+
+  $("#next-button").click(function () {
+    nextWord()
+  })
+
   $("#start div.start .btn").click(function () {
-
-    let word = classes[Math.floor(Math.random() * classes.length)];
+    word = classes[Math.floor(Math.random() * classes.length)];
     $("#word").text(word);
-
     let start_logo = $("#start div.start img")
     let x = start_logo.offset().left;
     let y = start_logo.offset().top;
@@ -62,7 +95,7 @@ $(document).ready(function () {
 
     $("body").attr("id", "game");
 
-    let game_logo = $("#game div.game img")
+    let game_logo = $("#game div.game img#logo")
     game_logo.css('top', y);
     game_logo.css('left', x);
     game_logo.css('width', width);
